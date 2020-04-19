0e7700b6d1723d738e67fb3efa05654a918ab6d8
Wang Chengke <wongck@pku.edu.cn>
2020-04-19 21:39:16 +0800

    [minijava & j2p] read from argv[0]

--- src/J2P.java
+++ src/J2P.java
@@ -8,6 +8,6 @@ public class J2P {
 
 	public static void main(String []args) throws Exception {
 		Emitter e = new Emitter(false);
-		TypeCheck.TypeCheck(e);
+		TypeCheck.TypeCheck(e, args.length > 0 ? args[0] : null);
 	}
 }
