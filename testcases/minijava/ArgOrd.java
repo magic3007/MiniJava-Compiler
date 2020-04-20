5e838bf8098f5160971f3934e2d112e982c0fd4f
Wang Chengke <wongck@pku.edu.cn>
2020-04-20 13:06:07 +0800

    [j2p] add test: evaluation order of arguments

---
+++ testcases/minijava/ArgOrd.java
@@ -0,0 +1,23 @@
+class argg {
+    public static void main(String[] a){
+        Test t;
+	    t = new Test();
+        System.out.println(t.start(t.f0(), t.f1()));
+    }
+}
+
+class Test {
+
+    public int f0() {
+        System.out.println(2);
+        return 1;
+    }
+    public int f1() {
+        System.out.println(2);
+        return 2;
+    }
+
+    public int start(int a, int b) {
+        return 3;
+    }
+}
